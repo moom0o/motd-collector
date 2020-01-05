55e74650f15a3989ae6fed3b873c6630a29e4a8e
moom0o <48740106+moom0o@users.noreply.github.com>
2020-01-04 21:35:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require("fs")
 const mcping = require('mc-ping-updated');
+const c = require("./config.json");
 
 setInterval(() => {
-mcping('2b2t.org', 25565, function(err, res) {
+mcping(c.ip, c.port, function(err, res) {
 	if (err) {
 		// Some kind of error
 		console.error(err);
@@ -13,7 +14,7 @@ mcping('2b2t.org', 25565, function(err, res) {
             if(adf.includes(res.description.extra[1].text.replace(/\n/g, ""))){
               return console.log(`motd is already in database`)
             }
-            fs.open('motds.txt', 'a', 666, function( e, id ) {
+            fs.open(c.file_location, 'a', 666, function( e, id ) {
                 fs.write( id, res.description.extra[1].text.replace(/\n/g, "") + ` (RED)` + "\n", null, 'utf8', function(){
                   fs.close(id, function(){
                     console.log('file is updated');
@@ -27,7 +28,7 @@ let adf = fs.readFileSync(`motds.txt`, 'utf-8')
 if(adf.includes(res.description.extra[1].text.replace(/\n/g, ""))){
   return console.log(`motd is already in database`)
 }
-fs.open('motds.txt', 'a', 666, function( e, id ) {
+fs.open(c.file_location, 'a', 666, function( e, id ) {
     fs.write( id, res.description.extra[1].text.replace(/\n/g, "") + "\n", null, 'utf8', function(){
       fs.close(id, function(){
         console.log('file is updated');
